dffced089417140f54b51b57ce0ff4f9be83e121
PedroExpedito <expeditocaf00@gmail.com>
2020-01-12 12:20:07 -0300

    12/01/2020

---
+++ src/headers/calculos.h
@@ -0,0 +1,12 @@
+#ifndef CALCULOS_H_INCLUED
+#define CALCULOS_H_INCLUED
+#define _PI 3.14159
+float quadrado(float x);
+float cubo(float x);
+
+int soma(int x,int n);
+int subtracao(int x,int n);
+int pot(int numero,int expoente);
+
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(void){
+    int *p,*a,*b,*c;
+    
+    while (1)
+    {   
+        printf("%p",p);
+        a = malloc(16711568);
+       
+    }
+    
+    
+}
