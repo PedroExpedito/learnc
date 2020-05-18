8d1480ddaeabfbeebab64bf4488a39f092c7ac1a
Leon Held <leonardo.held@grad.ufsc.br>
2020-05-18 06:08:17 -0700

    Added some info regarding arithmetic with mixed types

--- src/c/exercios/grupo1/g1e4.c
+++ src/c/exercios/grupo1/g1e4.c
@@ -9,11 +9,25 @@ C=(f-32)*(5/9).
 /* Enunciado confuso então fiz na versão "a" */
 
 #include <stdio.h>
+
 int main(void)
 {
   double x;
-  printf("Dijite o graus em fahrenheit para converter para celcius: ");
+  printf("Digite o graus em fahrenheit para converter para celcius: ");
   scanf("%lf", &x);
-  printf("em celsius %.2lf \n",(x - 32.0) * 5.0/9.0);
+  printf("em celsius %lf \n",(x - 32.0) * 5.0/9.0);
+
+  /*
+  x is double, if either operand is float, the other will be converted to float. 
+  Same thing happens in the 5/9 part, so there's no difference. Everything is 
+  implicited converted.
+  */
+  printf("Digite o graus em fahrenheit para converter para celcius: ");
+  scanf("%lf", &x);
+  //that's why the compiler doesn't warn when you use "%lf", because the result
+  //is expected to be a double.
+  printf("em celsius %f \n",(x - 32) * 5/9);
+
+
   return 0;
 }
